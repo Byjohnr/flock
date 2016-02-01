7c25ace59da8b620ff42ec5c2f9c6cefb193fa3b
ckoehler17 <ckoehler@iastate.edu>
2016-01-31 21:45:35 -0600

    Comment entity.

---
+++ src/main/java/cs309/data/Comment.java
@@ -0,0 +1,73 @@
+package cs309.data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by chasekoehler on 1/29/16.
+ */
+@Entity
+@Table(name = "user")
+public class Comment {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "owner_id")
+    private User owner;
+
+    @Column(name = "event_id")
+    private Event event;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "time")
+    private Date dateCreated;
+
+    public Comment() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return owner;
+    }
+
+    public void setUser(User owner) {
+        this.owner = owner;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Date getTime() {
+        return dateCreated;
+    }
+
+    public void setTime(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
