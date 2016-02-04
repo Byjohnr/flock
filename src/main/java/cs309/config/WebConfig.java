63eb9fe0c2010abcd9c4fcd00fa1af37e6e4bee2
jeffreyh <jeffreyh@iastate.edu>
2016-02-04 10:21:22 -0600

    removed unused imports

--- src/main/java/cs309/config/WebConfig.java
+++ src/main/java/cs309/config/WebConfig.java
@@ -4,10 +4,6 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.templateresolver.FileTemplateResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
-import org.thymeleaf.templateresolver.TemplateResolver;
-
 /**
  * Created by jeffrey on 1/22/16.
  */
