254b41064b69207e3975fb5c0ab3e20d1b744502
John <jruby@iastate.edu>
2016-02-03 22:57:32 -0600

    Continued work on  Notification Entity(left to do:finish yaml)

---
+++ src/main/java/cs309/service/NotificationService.java
@@ -0,0 +1,22 @@
+package cs309.service;
+
+import cs309.data.Notification;
+import cs309.repo.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by John on 2/3/2016.
+ */
+@Service
+public class NotificationService {
+    @Autowired
+    private NotificationRepository NotificationRepository;
+
+    public List<Notification> getNotifications() {
+        return NotificationRepository.findAll();
+    }
+
+}
