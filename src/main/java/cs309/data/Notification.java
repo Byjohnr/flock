03ccb26962c3e6baf50c9e412049509fe4d6e5c0
John <jruby@iastate.edu>
2016-02-03 21:50:30 -0600

    Created the Notification Entity

---
+++ src/main/java/cs309/data/Notification.java
@@ -0,0 +1,88 @@
+package cs309.data;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.jar.Attributes;
+
+/**
+ * Created by John on 2/3/2016.
+ */
+
+@Entity
+@Table(name = "Notification")
+
+public class Notification {
+
+    @Id
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id", referencedColumnName = "id")
+    private User receiver;
+
+    @ManyToOne
+    @JoinColumn(name = "creator_id", referencedColumnName = "id")
+    private User creator;
+
+    @Column(name = "type")
+    private Integer type;
+
+    @Column(name = "type_id")
+    private Integer type_id;
+
+    @Column(name = "time")
+    private Date dateCreated;
+
+    public Notification() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getType_id() {
+        return type_id;
+    }
+
+    public void setType_id(Integer type_id) {
+        this.type_id = type_id;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
