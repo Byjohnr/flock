f0fc4a85d537a496bb7a4797db7007d191109dc6
Chase <ckoehler@iastate.edu>
2016-02-03 21:41:21 -0600

    Just doing some testing with React

---
+++ src/main/resources/static/js/components/ckTest2.js
@@ -0,0 +1,31 @@
+/**
+ * Created by chasekoehler on 2/2/16.
+ */
+
+// tutorial2.js
+var CommentList = React.createClass({
+    render: function() {
+        var commentNodes = this.props.data.map(function(comment) {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+        });
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
+            </div>
+        );
+    }
+});
---
+++ src/main/resources/static/js/components/ckTest3.js
@@ -0,0 +1,15 @@
+/**
+ * Created by chasekoehler on 2/2/16.
+ */
+
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+});
