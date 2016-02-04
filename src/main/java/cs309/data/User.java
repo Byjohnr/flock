244011a1b815186dc37804c183c26c949bf34d24
Thomas <tstack@iastate.edu>
2016-02-04 12:01:43 -0600

    Commit of initial expanded User entity, to be continued

--- src/main/java/cs309/data/User.java
+++ src/main/java/cs309/data/User.java
@@ -1,6 +1,8 @@
 package cs309.data;
 
+import javax.imageio.ImageIO;
 import javax.persistence.*;
+
 @Entity
 @Table(name = "user")
 public class User {
@@ -14,11 +16,26 @@ public class User {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "current_city")
+    private String currentCity;
+
+//    This should be looked into for doing location
+//    private Long cityXCoordinate;
+//    private Long cityYCoordinate;
+
+
+//    Figure out how this works
+//    private ImageIO profilePicture;
+
+//    Look into doing connections
+//    private Connection connections
 
     public User(){
-
     }
-
 
     public Long getId() {
         return id;
@@ -42,5 +59,41 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCurrentCity() {
+        return currentCity;
+    }
+
+    public void setCurrentCity(String currentCity) {
+        this.currentCity = currentCity;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", description='" + description + '\'' +
+                ", currentCity='" + currentCity + '\'' +
+                '}';
     }
 }
