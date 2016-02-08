f488024d8456f5458ce0a2e3acff9c5cf3cb065a
Thomas Stackhouse <tstack@iastate.edu>
2016-02-08 01:10:14 -0600

    After working commit

---
+++ src/main/java/cs309/controller/UserController.java
@@ -0,0 +1,22 @@
+package cs309.controller;
+
+import cs309.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Thomas on 2/8/2016.
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/account")
+    public String userAccountPage() {
+        return "index";
+    }
+}
