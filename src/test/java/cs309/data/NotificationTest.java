f8df441cfbe8aeb24f272e46f2ccdde93c3ddcfa
John <jruby@iastate.edu>
2016-02-08 23:37:25 -0600

    Created Notification Test classes(will push once tests are in)

--- src/test/java/cs309/data/NotificationTest.java
+++ src/test/java/cs309/data/NotificationTest.java
@@ -1,5 +1,8 @@
 package cs309.data;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,4 +10,68 @@ import static org.junit.Assert.*;
  */
 public class NotificationTest {
 
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @Test
+    public void testSetId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetReceiver() throws Exception {
+
+    }
+
+    @Test
+    public void testSetReceiver() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCreator() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCreator() throws Exception {
+
+    }
+
+    @Test
+    public void testGetType() throws Exception {
+
+    }
+
+    @Test
+    public void testSetType() throws Exception {
+
+    }
+
+    @Test
+    public void testGetType_id() throws Exception {
+
+    }
+
+    @Test
+    public void testSetType_id() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDateCreated() throws Exception {
+
+    }
+
+    @Test
+    public void testSetDateCreated() throws Exception {
+
+    }
 }
---
+++ src/test/java/service/NotificationServiceUTest.java
@@ -0,0 +1,18 @@
+package service;
+
+import cs309.data.Notification;
+import cs309.repo.NotificationRepository;
+import cs309.service.NotificationService;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import util.MockData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by John on 2/8/2016.
+ */
+public class NotificationServiceUTest {
+}
