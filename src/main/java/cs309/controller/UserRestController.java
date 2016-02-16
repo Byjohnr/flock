42fda11179041d23ac5e95de4748e1c13a6cd2bd
jeffreyh <jeffreyh@iastate.edu>
2016-02-15 22:48:01 -0600

    Added spring security to the project, its getting there!

--- src/main/java/cs309/controller/UserRestController.java
+++ src/main/java/cs309/controller/UserRestController.java
@@ -3,6 +3,7 @@ package cs309.controller;
 import cs309.data.User;
 import cs309.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,13 @@ public class UserRestController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @RequestMapping("/api/user/create")
     public String createUser(@RequestBody User user) {
 //        TODO jeffreyh 2/8/16 validation and encrypting password
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userService.saveUser(user);
         return "success";
     }
