3ea5351e1cc58939f1d281e459a438c9fc195b45
Chase <ckoehler@iastate.edu>
2016-02-16 22:40:38 -0600

    I am working on the edit event function.  Trying to figure out bootstrap so I can click an edit button and a modal window pops up.

--- src/main/resources/static/js/components/EventPage.js
+++ src/main/resources/static/js/components/EventPage.js
@@ -1,14 +1,33 @@
 /**
  * Created by chasekoehler on 2/8/16.
  */
-
 var EventPage = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <div className="row">
                 <div className="col-sm-5 col-sm-offset-3">
                     <h1 className="text-center"> Event </h1>
                     <EventElements/>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#EditModal">
+                        Launch demo modal
+                    </button>
+                    <div class="modal fade" id="EditModal" tabindex="-1" role="dialog" aria-labelledby="EditModalLabel">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                                </div>
+                                <div class="modal-body">
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
