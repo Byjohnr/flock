bea8b8c4a6f72fde0fb8ab5ddcb6ad797767293c
John <jruby@iastate.edu>
2016-02-22 20:34:28 -0600

    Done for the night, next up is to put actual useful data in table

--- src/main/resources/static/js/components/NotificationList.js
+++ src/main/resources/static/js/components/NotificationList.js
@@ -14,11 +14,11 @@ var NotificationList = React.createClass({
     render: function() {
         console.log('rendering');
         if(this.state.notifications === undefined) {
-            return <div> Loading <i className="fa fa-spin fa-refresh text-center"/> </div>;
+            return <div> Loading <i className="fa fa-spin fa-refresh -align-center"/> </div>;
         }
 
         var notificationNodes = this.state.notifications.map(function (notification){
-            return <tr key={notification.id}>  <td>{notification.type}</td><td className="text-center">{notification.notificationName}</td></tr>        });
+            return <tr key={notification.id}>  <td>{notification.type}</td><td>{notification.notificationName}</td></tr>});
 
         if(notificationNodes.length === 0) {
             return(<div className="text-center">You have no notifications!</div>)
@@ -28,14 +28,14 @@ var NotificationList = React.createClass({
                 <NavBar/>
                 <table className="table table-hover">
                     <thead>
-                        <tr>
-                            <th>
-                                Type
-                            </th>
-                            <th>
-                                Name
-                            </th>
-                        </tr>
+                    <tr>
+                        <th>
+                            Type
+                        </th>
+                        <th>
+                            Name
+                        </th>
+                    </tr>
                     </thead>
                     <tbody>
                     {notificationNodes}
