ba07fecabb8231abf4cfcfa9af221304c2378f87
John <jruby@iastate.edu>
2016-03-04 00:30:22 -0600

    bind/delete notification isn't working

--- src/main/resources/static/js/components/NotificationList.js
+++ src/main/resources/static/js/components/NotificationList.js
@@ -5,16 +5,16 @@
 
 var NotificationList = React.createClass({
     mixins: [Reflux.connect(NotificationStore, 'notifications')],
-    getInitialState : function () {
+    getInitialState: function () {
         return {notifications: undefined};
     },
-    componentDidMount : function () {
+    componentDidMount: function () {
         console.log('componentMounted');
         NotificationActions.fetchNotifications();
     },
-    handleDelete : function(id){
-        NotificationActions.deleteNotification(id);
+    handleDelete: function () {
         console.log('deleting');
+       // NotificationActions.deleteNotification(id);onClick={handleDelete(notification.id)
     },
     render: function () {
         console.log('rendering');
@@ -22,14 +22,14 @@ var NotificationList = React.createClass({
             return <div> Loading <i className="fa fa-spin fa-refresh align-center"/></div>;
         }
         var notificationNodes = this.state.notifications.map(function (notification) {
-            var onClick = this.handleDelete.bind(this,notification.id);
+            //var onClick = this.handleDelete.bind();
             return (<tr key={notification.id}>
                 <td>{notification.type}</td>
                 <td>{notification.message}</td>
                 <td><a className="btn btn-info active" href={notification.url} role="button">View</a>
                     <button type="button" className="btn btn-success">Accept</button>
                     <button type="button" className="btn btn-primary">Decline</button>
-                    <button type="button" className="btn btn-danger" onClick={onClick}>Delete</button>
+                    <button type="button" className="btn btn-danger">Delete</button>
                 </td>
             </tr>);
         });
