ab031bb7d0c0082578156d464e7464fce707d9aa
Thomas Stackhouse <tstack@iastate.edu>
2016-03-03 22:03:58 -0600

    Deleted unused imports

--- src/main/java/cs309/Application.java
+++ src/main/java/cs309/Application.java
@@ -1,11 +1,7 @@
 package cs309;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class Application {
