8089f4307095ec56ba1e04e29f7164f62ee72a21
Thomas Stackhouse <tstack@iastate.edu>
2016-03-07 16:21:07 -0600

    Cleaned up some extra spaces, got rid of some comments, and made security disabled on this branch temporarily for development. Will re-enable security for merging into master

--- src/main/resources/static/js/stores/pictureStore.js
+++ src/main/resources/static/js/stores/pictureStore.js
@@ -17,29 +17,7 @@ var PictureStore = Reflux.createStore({
             }
         });
     },
-    onGetUserInformation: function () {
-        $.ajax({
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            url: '/api/user/info',
-            type: 'GET',
-            success: this.returnInformationSuccess,
-            error: function () {
-                console.log("error getting user information from the backend");
-            }
-        });
-    },
-    returnInformationSuccess: function (data) {
-        this.trigger(data);
-    },
-    onGetOtherUserInfo: function() {
-        var id = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
-        $.ajax({
-            url: '/api/user/' + id,
-            dataType:'json',
-            success : this.returnInformationSuccess
-        })
+    handleFile: function (file) {
+        console.log("file returned");
     }
 });
