75498d3ec497190254a720d5e9c135f345700fde
jeffreyh <jeffreyh@iastate.edu>
2016-03-31 20:18:25 -0500

    Finished up my the entities, going to make a pull request to get feedback about them

--- src/main/java/cs309/data/ChatGroup.java
+++ src/main/java/cs309/data/ChatGroup.java
@@ -1,7 +1,6 @@
 package cs309.data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.sql.Date;
 import java.util.List;
 
@@ -9,16 +8,84 @@ import java.util.List;
 @Table(name = "chat_group")
 public class ChatGroup {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Integer id;
+
+    @OneToMany(mappedBy = "chatGroup")
     private List<ChatUser> chatUsers;
 
-    private List<ChatMsg> chatMsgs;
+    @OneToMany(mappedBy = "chatGroup")
+    private List<ChatMessage> chatMessages;
 
+    @ManyToOne
+    @JoinColumn(name = "chat_admin_id", referencedColumnName = "id")
     private User chatAdmin;
 
+    @Column(name = "date_created")
     private Date dateCreated;
 
+    @Column(name = "last_updated")
     private Date lastUpdated;
 
+    @Column(name = "chat_name")
     private String chatName;
 
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<ChatUser> getChatUsers() {
+        return chatUsers;
+    }
+
+    public void setChatUsers(List<ChatUser> chatUsers) {
+        this.chatUsers = chatUsers;
+    }
+
+    public List<ChatMessage> getChatMessages() {
+        return chatMessages;
+    }
+
+    public void setChatMessages(List<ChatMessage> chatMessages) {
+        this.chatMessages = chatMessages;
+    }
+
+    public User getChatAdmin() {
+        return chatAdmin;
+    }
+
+    public void setChatAdmin(User chatAdmin) {
+        this.chatAdmin = chatAdmin;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public String getChatName() {
+        return chatName;
+    }
+
+    public void setChatName(String chatName) {
+        this.chatName = chatName;
+    }
 }
--- src/main/java/cs309/data/ChatUser.java
+++ src/main/java/cs309/data/ChatUser.java
@@ -1,18 +1,68 @@
 package cs309.data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "chat_invite")
+@Table(name = "chat_group_user")
 public class ChatUser {
 
-    private User userInvited;
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Integer id;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_group_id", referencedColumnName = "id")
     private ChatGroup chatGroup;
 
-    private List<ChatMsg> chatMsgs;
+    @OneToMany(mappedBy = "messageSender")
+    private List<ChatMessage> chatMessages;
 
+    @Column(name = "status")
     private Integer status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public ChatGroup getChatGroup() {
+        return chatGroup;
+    }
+
+    public void setChatGroup(ChatGroup chatGroup) {
+        this.chatGroup = chatGroup;
+    }
+
+    public List<ChatMessage> getChatMessages() {
+        return chatMessages;
+    }
+
+    public void setChatMessages(List<ChatMessage> chatMessages) {
+        this.chatMessages = chatMessages;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 }
