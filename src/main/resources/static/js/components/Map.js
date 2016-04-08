be24085231205e33f06e47105a3b151299918a2b
Chase <ckoehler@iastate.edu>
2016-04-08 17:26:25 -0500

    Tried to get the map to display the events.

--- src/main/resources/static/js/components/Map.js
+++ src/main/resources/static/js/components/Map.js
@@ -1,18 +1,41 @@
 var Map= React.createClass({
+    getInitialState: function() {
+        return {googleMap: undefined,
+        geocoder: undefined}
+    },
     componentDidMount: function() {
         console.log("Mounted");
         google.maps.event.addDomListener(window, 'load', this.initMap());
+        this.codeAddresses();
     },
     initMap : function() {
         var myLatLng = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('daMap'), {
+        this.state.geocoder = new google.maps.Geocoder();
+        this.state.googleMap = new google.maps.Map(document.getElementById('daMap'), {
             center: {lat: 42.018018, lng: -93.660742},
             zoom: 5});
 
         var marker = new google.maps.Marker({
             position: myLatLng,
-            map: map,
+            map: this.state.googleMap,
             title: 'Hello World!'
+        });
+    },
+    codeAddresses : function() {
+        var parent = this;
+        var markerNodes = this.props.events.map(function (event) {
+            var address = event.location;
+            this.state.geocoder.geocode( { 'address': address}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    parent.state.googleMap.setCenter(results[0].geometry.location);
+                    var marker = new google.maps.Marker({
+                        map: parent.state.googleMap,
+                        position: results[0].geometry.location
+                    });
+                } else {
+                    alert("Geocode was not successful for the following reason: " + status);
+                }
+            });
         });
     },
     render: function() {
--- src/main/resources/static/js/components/MapPage.js
+++ src/main/resources/static/js/components/MapPage.js
@@ -1,9 +1,18 @@
 var MapPage = React.createClass({
+    mixins: [Reflux.connect(EventStore,'events')],
+    getInitialState: function() {
+        console.log("initial");
+        return {events: undefined};
+    },
+    componentDidMount: function() {
+        console.log("Mounted2");
+        EventActions.listEvents();
+    },
     render: function() {
         return(
             <div>
                 <NavBar />
-                <Map />
+                <Map events={this.state.events}/>
             </div>
         );
     }
