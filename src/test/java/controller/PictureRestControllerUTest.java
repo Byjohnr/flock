5b10618aeb93a5aa90aa613f2db076eff098ec39
Chase <ckoehler@iastate.edu>
2016-04-11 19:13:40 -0500

    Made some tests for the join and picture features.

--- src/test/java/controller/PictureRestControllerUTest.java
+++ src/test/java/controller/PictureRestControllerUTest.java
@@ -103,6 +103,37 @@ public class PictureRestControllerUTest extends UnitTestBase {
         verifyNoMoreInteractions(userService, pictureFileService);
     }
 
+    @Test
+    public void eventPictureGetter() throws Exception {
+        PictureFile returnedPictureFile = new PictureFile("returnedPicture.png", 1, null, "The Easter Bunny's map of where he hid eggs.");
+        when(pictureFileService.getPictureFileByEventId(anyInt())).thenReturn(returnedPictureFile);
+
+        ResultActions results = this.mockMvc.perform(get("/api/event_picture/{eventId}", 1).accept(MediaType.TEXT_HTML).principal(mock(Principal.class)));
+
+        results
+                .andExpect(status().isOk())
+                .andExpect(content().string(returnedPictureFile.getPicture()));
+
+        verify(pictureFileService, times(1)).getPictureFileByEventId(1);
+    }
+
+    @Test
+    public void eventPictureUpload() throws Exception {
+        PictureFile returnedPictureFile = new PictureFile("returnedPicture.png", 1, null, "The Easter Bunny's map of where he hid eggs.");
+        when(pictureFileService.savePictureFileForEvent(returnedPictureFile.getFileName(), 1, returnedPictureFile.getPicture())).thenReturn(returnedPictureFile);
+
+        ResultActions results = this.mockMvc.perform(post("/api/picture_upload/event_picture/1").accept(MediaType.TEXT_HTML).principal(mock(Principal.class))
+                .param("picture", returnedPictureFile.getPicture()).param("fileName", returnedPictureFile.getFileName()));
+
+        results
+                .andExpect(status().isOk())
+                .andExpect(content().string(returnedPictureFile.getPicture()));
+        
+        verify(pictureFileService, times(1)).savePictureFileForEvent(returnedPictureFile.getFileName(), 1, returnedPictureFile.getPicture());
+        verifyNoMoreInteractions(userService, pictureFileService);
+    }
+
+
     @After
     public void resetMocks() {
         reset(userService, pictureFileService);
