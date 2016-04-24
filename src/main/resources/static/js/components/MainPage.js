e30e746b528b88f37e28e25f8be89bef9d5686ef
Thomas <tstack@iastate.edu>
2016-04-23 20:42:42 -0500

    Added a map to the mainPage with a basic marker

--- src/main/resources/static/js/components/MainPage.js
+++ src/main/resources/static/js/components/MainPage.js
@@ -1,21 +1,42 @@
 var MainPage = React.createClass({
-    mixins: [Reflux.connect(UserStore,'user')],
-   render: function() {
-       //TODO jeffreyh 2/8/16 still need to create the anonymous user part of the main page
-       if(this.state.user != undefined) {
-
-       }
-       return(
-
-         <div className="row">
-             <NavBar  hideInfo="true" />
-            <div className="col-sm-7"></div>
-             <div className="col-sm-5">
-                 <RegisterUser />
-             </div>
-         </div>
-       );
-   }
+    mixins: [Reflux.connect(UserStore, 'user')],
+    componentDidMount: function () {
+        google.maps.event.addDomListener(window, 'load', this.initMap());
+    },
+    initMap: function () {
+        var myLatLng = {lat: 42.0308, lng: -93.6319};
+        var mapOptions = {
+            zoom: 8,
+            center: myLatLng
+        };
+        var map = new google.maps.Map(document.getElementById("anonymous_user_map"), mapOptions);
+        this.addMarker(map, myLatLng);
+    },
+    addMarker: function (map, latLng) {
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map//,
+            //title:"This is the city of Ames!!"
+        });
+        // google.maps.event.addListener(marker, 'click', function () {
+        //     console.log("You clicked " + marker.title);
+        // })
+    },
+    render: function () {
+        return (
+            <div className="row">
+                <NavBar hideInfo="true"/>
+                <div className="col-sm-6">
+                    <h2 className="text-center">See events near you!</h2>
+                    <div id="anonymous_user_map"
+                         style={{height: '500px', width: '90%', marginLeft: '5%', marginRight: '5%', borderWidth: 1, borderStyle: 'solid'}}></div>
+                </div>
+                <div className="col-sm-6">
+                    <RegisterUser />
+                </div>
+            </div>
+        );
+    }
 });
 
 ReactDOM.render(<MainPage />, document.getElementById("main_page"));
