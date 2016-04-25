c063731799572befb4fa7900700b44e50db160b1
John <jruby@iastate.edu>
2016-04-25 10:46:29 -0500

    commit

--- src/main/resources/static/js/components/Map.js
+++ src/main/resources/static/js/components/Map.js
@@ -1,39 +1,44 @@
-var Map= React.createClass({
+var Map = React.createClass({
     mixins: [Reflux.connect(UserStore, 'user')],
-    getInitialState: function() {
+    getInitialState: function () {
         return {googleMap: undefined, user: undefined}
     },
-    componentDidMount: function() {
-        google.maps.event.addDomListener(window, 'load', this.initMap());
-        this.getLocations();
+    componentDidMount: function () {
         UserActions.getUserInformation();
+        //google.maps.event.addDomListener(window, 'load', this.initMap());
+       // this.getLocations();
     },
-    initMap : function() {
-        var myLatLng = {lat: -25.363, lng: 131.044};
+    initMap: function () {
+        var parent = this;
+        var myLatLng = {lat: -25.363, lng: 131.044};;
         var zoom = 5;
+        var result;
         var initGeoCoder = new google.maps.Geocoder();
-        if (this.state.user.currentCity != undefined){
-            initGeoCoder.geocode({'address': this.state.user.currentCity}), function (results, status) {
-                if (status == google.maps.GeocoderStatus.OK){
-                zoom = 7
+        if (this.state.user.currentCity != undefined) {
+            var address = this.state.user.currentCity;
+            initGeoCoder.geocode({'address': address}, function (results, status) {
+                result= results;
+                if (status == google.maps.GeocoderStatus.OK) {
+                    zoom = 2;
                 }
-                else{
-                    address = myLatLng;
-                }
-            }
-            zoom = 7;
+            });
         }
-        else{
+        else {
             address = myLatLng;
             zoom = 5;
         }
-
-        console.log(this.state.geocoder);
-        this.setState({googleMap : new google.maps.Map(document.getElementById('daMap'), {
-            center: {address},
-            zoom: zoom})});
+        console.log(this.state.user.currentCity);
+        console.log(initGeoCoder);
+        console.log(result[0]);
+       // this.setState({
+            googleMap: new google.maps.Map(document.getElementById('daMap'), {
+                map: parent.state.googleMap,
+                center: {position: result[0].geometry.location},
+                zoom: zoom
+            })
+        //});
     },
-    getLocations : function() {
+    getLocations: function () {
         var parent = this;
         var geocoder = new google.maps.Geocoder();
         if (parent.props.events.location != undefined) {
@@ -61,11 +66,12 @@ var Map= React.createClass({
             });
         }
     },
-    render: function() {
+    render: function () {
         if (this.state.user != undefined){
-            UserActions.getUserInformation();
+            google.maps.event.addDomListener(window, 'load', this.initMap());
+            this.getLocations();
         }
-        return(
+        return (
             <div id="daMap" style={{height: this.props.height, width: this.props.width}}>
             </div>
         );
